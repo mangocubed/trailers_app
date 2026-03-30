9e2c34406575905acb7fb831f4b5a2f87374dc6e
Javier E. <jaedns@gmail.com>
2026-03-30 17:11:24 -0300

    Add ad support with a webview

--- lib/config.dart
+++ lib/config.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/foundation.dart';
 
 class Config {
+  static final _adUrl = const String.fromEnvironment('AD_URL');
+
+  static final Uri? adUrl = _adUrl.isNotEmpty ? Uri.parse(_adUrl) : null;
+
+  static final adSize = const String.fromEnvironment('AD_SIZE').split('x').map((s) => double.parse(s)).toList();
+
   static final Uri identityApiUrl = Uri.parse(
     const String.fromEnvironment('IDENTITY_API_URL', defaultValue: 'http://127.0.0.1:8005'),
   );
 
-  static final String identityClientId = const String.fromEnvironment('IDENTITY_CLIENT_ID', defaultValue: '');
+  static final String identityClientId = const String.fromEnvironment('IDENTITY_CLIENT_ID');
 
   static final Uri identityRedirectUrl = Uri.parse(
     const String.fromEnvironment(
---
+++ lib/screens/show_ad_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+import '../config.dart';
+
+class ShowAdScreen extends StatelessWidget {
+  const ShowAdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                width: Config.adSize.first,
+                height: Config.adSize.last,
+                child: InAppWebView(
+                  initialUrlRequest: URLRequest(url: WebUri.uri(Config.adUrl!)),
+                  initialSettings: InAppWebViewSettings(
+                    disableContextMenu: true,
+                    disableHorizontalScroll: true,
+                    disableVerticalScroll: true,
+                    horizontalScrollBarEnabled: false,
+                    javaScriptEnabled: true,
+                    supportZoom: false,
+                    thirdPartyCookiesEnabled: true,
+                    verticalScrollBarEnabled: false,
+                    overScrollMode: OverScrollMode.NEVER,
+                    allowsBackForwardNavigationGestures: false,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: Chip(label: Text('Advertisement')),
+          ),
+        ],
+      ),
+    );
+  }
+}
