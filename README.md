8d3138c11127a261b4975edee85a77af133fde71
Javier E. <jaedns@gmail.com>
2026-03-30 18:48:44 -0300

    Update default config values for production environment

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ Movies and TV Series recommendations.
 
 ## Environment variables
 
-| Name                  | Type   | Default                      |
-| --------------------- | ------ | ---------------------------- |
-| IDENTITY_API_URL      | String | http://127.0.0.1:8005        |
-| IDENTITY_CLIENT_ID    | String |                              |
-| IDENTITY_REDIRECT_URL | String | app.mango3.trailers://oauth  |
-| IDENTITY_URL          | String | http://127.0.0.1:8000        |
-| TRAILERS_API_URL      | String | http://127.0.0.1:8015        |
-| TRAILERS_URL          | String | https://trailers.mango3.app/ |
+| Name                  | Type   | Default                             |
+| --------------------- | ------ | ----------------------------------- |
+| AD_SIZE               | String | 336x280                             |
+| AD_URL                | String | https://ads.mango3.app/336x280.html |
+| IDENTITY_API_URL      | String | https://api.id.mango3.app/          |
+| IDENTITY_CLIENT_ID    | String |                                     |
+| IDENTITY_REDIRECT_URL | String | app.mango3.trailers://oauth         |
+| IDENTITY_URL          | String | https://id.mango3.app/              |
+| TRAILERS_API_URL      | String | http://api.trailers.mango3.app/     |
+| TRAILERS_URL          | String | https://trailers.mango3.app/        |
--- lib/config.dart
+++ lib/config.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/foundation.dart';
 
 class Config {
-  static final _adUrl = const String.fromEnvironment('AD_URL');
+  static final _adUrl = const String.fromEnvironment('AD_URL', defaultValue: 'https://ads.mango3.app/336x280.html');
+
+  static final adSize = const String.fromEnvironment(
+    'AD_SIZE',
+    defaultValue: '336x280',
+  ).split('x').map((s) => double.parse(s)).toList();
 
   static final Uri? adUrl = _adUrl.isNotEmpty ? Uri.parse(_adUrl) : null;
 
-  static final adSize = const String.fromEnvironment('AD_SIZE').split('x').map((s) => double.parse(s)).toList();
-
   static final Uri identityApiUrl = Uri.parse(
-    const String.fromEnvironment('IDENTITY_API_URL', defaultValue: 'http://127.0.0.1:8005'),
+    const String.fromEnvironment('IDENTITY_API_URL', defaultValue: 'https://api.id.mango3.app/'),
   );
 
   static final String identityClientId = const String.fromEnvironment('IDENTITY_CLIENT_ID');
@@ -21,11 +24,11 @@ class Config {
   );
 
   static final Uri identityUrl = Uri.parse(
-    const String.fromEnvironment('IDENTITY_URL', defaultValue: 'http://127.0.0.1:8000'),
+    const String.fromEnvironment('IDENTITY_URL', defaultValue: 'https://id.mango3.app/'),
   );
 
   static final Uri trailersApiUrl = Uri.parse(
-    const String.fromEnvironment('TRAILERS_API_URL', defaultValue: 'http://127.0.0.1:8015'),
+    const String.fromEnvironment('TRAILERS_API_URL', defaultValue: 'https://api.trailers.mango3.app/'),
   );
 
   static final Uri trailersUrl = Uri.parse(
